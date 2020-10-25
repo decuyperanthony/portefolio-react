0b657c32fed5b50f4e877cd3f4f9e03b30cda34a
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-10-25 17:16:45 +0100

    Update front.js

--- src/App/Technologie/front.js
+++ src/App/Technologie/front.js
@@ -49,7 +49,11 @@ const useStyles = makeStyles((theme) => ({
         fontSize: '12px',
         margin: 0,
         marginBottom: '0.5em'
-    }
+    },
+    [theme.breakpoints.up('sm')]: {
+        width: '48%',
+        marginRight: '0.25em'
+    },
     },
     containerTechno: {
         display: 'flex',
