ef3ea8fcc9b605553ca5ad36ab13df9dde2948b4
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-26 22:54:02 +0100

    paginate done :100:

--- src/App/Projects/index.js
+++ src/App/Projects/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Pagination from '@material-ui/lab/Pagination';
+
 // == components
 import Title from './Title';
 import Project from './Projects';
@@ -7,17 +9,34 @@ import Project from './Projects';
 // == data
 import projectsData from '../../data/projectsData';
 
-const Projects= () => {
-    const projectsDataLength = projectsData.length
-    const ProjectCardJSX = projectsData.map((p) => {
-        return(
-            <Project key={p.id + 22} projectData={p}/>
-        )
-    })
+const Projects = () => {
+    const [firstItem, setFirstItem] = React.useState(0);
+    const [lastItem, setLastItem] = React.useState(3);
+    const [page, setPage] = React.useState(1);
+    const projectsDataLength = projectsData.length;
+    const countPagination = Math.ceil(projectsDataLength / 3);
+    const mySplitProjectTable = [...projectsData];
+    const handleChange = (event, value) => {
+        setPage(value);
+        setFirstItem(value * 3 - 3);
+        setLastItem(value * 3);
+    };
+    const ProjectCardJSX = mySplitProjectTable.slice(firstItem, lastItem )
+        .map((p) => {
+                return(
+                    <Project key={p.id + 22} projectData={p}/>
+                )
+        });
     return (
        <>
          <Title totalOfProjects={projectsDataLength}/>
          {ProjectCardJSX}
+         <Pagination
+            count={countPagination}
+            color="primary"
+            page={page}
+            onChange={handleChange}
+          />
        </>
     );
 };
